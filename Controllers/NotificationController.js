460c67dbc5a6b2f318b8d97d7fc752cfdf70fb46
mizairyaktharmf <mizairyakthar@gmail.com>
2025-11-15 01:02:02 +0400

    notificationcontrollor for control nofi

---
+++ Controllers/NotificationController.js
@@ -0,0 +1,20 @@
+import Notification from "../models/Notification.js";
+
+export const createNotification = async (message, type, user="admin") => {
+  await Notification.create({ message, type, user });
+};
+
+export const getNotifications = async (req, res) => {
+  const notes = await Notification.find().sort({ createdAt: -1 });
+  res.json(notes);
+};
+
+export const markAsRead = async (req, res) => {
+  await Notification.findByIdAndUpdate(req.params.id, { read: true });
+  res.json({ success: true });
+};
+
+export const markAllRead = async (req, res) => {
+  await Notification.updateMany({}, { read: true });
+  res.json({ success: true });
+};
