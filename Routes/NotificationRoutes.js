a4e8ae288451f7545009ab31cda1072a13ea2a6f
MF Mizairy Akthar <mizairyakthar@gmail.com>
2025-11-15 12:53:03 +0400

    Update NotificationRoutes.js

--- Routes/NotificationRoutes.js
+++ Routes/NotificationRoutes.js
@@ -1,24 +1,14 @@
-// Routes/NotificationRoutes.js
 import express from "express";
 import {
   getNotifications,
-  createNotificationAPI,
   markAsRead,
-  markAllRead,
+  markAllRead
 } from "../Controllers/NotificationController.js";
 
 const router = express.Router();
 
-// Create notification (frontend or system call)
-router.post("/", createNotificationAPI);
-
-// Get all notifications
 router.get("/", getNotifications);
-
-// Mark single as read
 router.patch("/read/:id", markAsRead);
-
-// Mark all as read
 router.patch("/read-all", markAllRead);
 
 export default router;
