14b669d461ba12eea64c9f8b511ef70076624bd0
MF Mizairy Akthar <mizairyakthar@gmail.com>
2025-11-20 16:02:47 +0400

    Update RoleMiddleware.js

---
+++ Middleware/RoleMiddleware.js
@@ -0,0 +1,17 @@
+export const allowAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({
+      message: "Access denied. Admins only.",
+    });
+  }
+  next();
+};
+
+export const allowStaffOrAdmin = (req, res, next) => {
+  if (req.user.role !== "admin" && req.user.role !== "staff") {
+    return res.status(403).json({
+      message: "Access denied.",
+    });
+  }
+  next();
+};
