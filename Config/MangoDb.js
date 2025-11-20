6220217b1c5f81e0525695314c0163bd937bd8e1
MF Mizairy Akthar <mizairyakthar@gmail.com>
2025-11-20 16:12:05 +0400

    Update MangoDb.js

--- Config/MangoDb.js
+++ Config/MangoDb.js
@@ -3,8 +3,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected Successfully");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1);
